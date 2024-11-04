515063ff6339583b08993d69ce166499175bdb32
S-Safrin <133481766+S-Safrin@users.noreply.github.com>
2024-11-04 16:32:59 +0530

    completed

---
+++ index.js
@@ -0,0 +1,27 @@
+// function Calculate() {
+//     // let n1=parseFloat(document.getElementById("num1").value);
+//     // let operator=document.getElementById("operator").value
+//     // let n2=parseFloat(document.getElementById("num2").value);
+//     let n1=0;
+//     let n2=0;
+//     let operator;
+//     switch (operator) {
+//         case "+":
+//             result=n1+n2;
+//             break;
+//         case "-":
+//             result=n1-n2;
+//             break;
+//         case "*":
+//             result=n1*n2;
+//             break;
+//         case "/":
+//             result=n1/n2;
+//             break;
+//         default:
+//             result="Invalid one";
+//             break;
+//     }
+//     console.log(result);
+// }
+// Calculate()
